83f3a415a35d420c7403663a9d06c8a39404e2fa
Karthick J <karthick.j@ittiam.com>
2014-12-10 14:42:53 +0530

    Optimization of GPU fill input

--- vp9/encoder/vp9_egpu.c
+++ vp9/encoder/vp9_egpu.c
@@ -136,28 +136,88 @@ static void vp9_gpu_fill_rd_parameters(VP9_COMP *cpi, MACROBLOCK *const x) {
     rd_param_ptr->thresh_fact_newmv = cpi->rd.thresh_freq_fact[bsize][NEWMV];
   }
 }
+static void vp9_gpu_fill_input_block(VP9_COMP *cpi, const TileInfo *const tile,
+                                     GPU_INPUT *gpu_input,
+                                     int mi_row, int mi_col,
+                                     BLOCK_SIZE bsize) {
 
-static void vp9_gpu_fill_mv_input(VP9_COMP *cpi, const TileInfo * const tile) {
-  SPEED_FEATURES *const sf = &cpi->sf;
-  int mi_row, mi_col;
-  int mi_width, mi_height;
-  VP9_EGPU *egpu = &cpi->egpu;
   MACROBLOCK *const x = &cpi->mb;
   VP9_COMMON *const cm = &cpi->common;
   MACROBLOCKD *const xd = &x->e_mbd;
   MB_MODE_INFO *mbmi = &xd->mi[0]->mbmi;
-  MV_REFERENCE_FRAME ref_frame = LAST_FRAME;
+  int mi_width, mi_height;
+  const MV_REFERENCE_FRAME ref_frame = LAST_FRAME;
   int_mv *const candidates = mbmi->ref_mvs[ref_frame];
+  const int bsl = mi_width_log2(bsize);
+  const int pred_filter_search = cm->interp_filter == SWITCHABLE ?
+      (((mi_row + mi_col) >> bsl) +
+       get_chessboard_index(cm->current_video_frame)) & 0x1 : 0;
+
+  const int ms = num_8x8_blocks_wide_lookup[bsize] / 2;
+  const int force_horz_split = (mi_row + ms >= cm->mi_rows);
+  const int force_vert_split = (mi_col + ms >= cm->mi_cols);
+  if (force_horz_split | force_vert_split) {
+    return;
+  }
+
+  mi_width = num_8x8_blocks_wide_lookup[bsize];
+  mi_height = num_8x8_blocks_high_lookup[bsize];
+
+  set_mi_row_col(xd, tile, mi_row, mi_height,
+                 mi_col, mi_width,
+                 cm->mi_rows, cm->mi_cols);
+
+  mbmi->sb_type = bsize;
+
+  vp9_find_mv_refs_rt(cm, x, tile, xd->mi[0], ref_frame, candidates,
+                      mi_row, mi_col);
+
+  vp9_find_best_ref_mvs(xd, cm->allow_high_precision_mv, candidates,
+                        (int_mv *)&gpu_input->nearest_mv,
+                        (int_mv *)&gpu_input->near_mv);
+
+  clamp_mv2(&gpu_input->nearest_mv, xd);
+  clamp_mv2(&gpu_input->near_mv, xd);
+
+  gpu_input->do_newmv     = 1;
+  gpu_input->do_compute   = 1;
+  gpu_input->mode_context = mbmi->mode_context[ref_frame];
+
+  if(pred_filter_search)
+    gpu_input->filter_type = SWITCHABLE;
+  else
+    gpu_input->filter_type = EIGHTTAP;
+
+}
+
+static void vp9_gpu_fill_mv_input(VP9_COMP *cpi, const TileInfo * const tile) {
+  SPEED_FEATURES * const sf = &cpi->sf;
+  int mi_row, mi_col;
+  VP9_EGPU *egpu = &cpi->egpu;
+  VP9_COMMON * const cm = &cpi->common;
   GPU_INPUT *gpu_input_base;
   GPU_BLOCK_SIZE gpu_bsize;
+
+  if (!sf->partition_check) {
+    for (mi_row = tile->mi_row_start; mi_row < tile->mi_row_end; mi_row +=
+        MI_BLOCK_SIZE) {
+      for (mi_col = tile->mi_col_start; mi_col < tile->mi_col_end; mi_col +=
+          MI_BLOCK_SIZE) {
+        const int sb_row_index = mi_row / MI_SIZE;
+        const int sb_col_index = mi_col / MI_SIZE;
+        const int sb_index = (cm->sb_cols * sb_row_index + sb_col_index);
+        cm->is_background_map[sb_index] = is_background(cpi, tile, mi_row,
+                                                        mi_col);
+      }
+    }
+  }
 
   for (gpu_bsize = 0; gpu_bsize < GPU_BLOCK_SIZES; ++gpu_bsize) {
     const BLOCK_SIZE bsize = get_actual_block_size(gpu_bsize);
     const int mi_row_step = num_8x8_blocks_high_lookup[bsize];
     const int mi_col_step = num_8x8_blocks_wide_lookup[bsize];
 
-    egpu->acquire_input_buffer(cpi, gpu_bsize, (void **)&gpu_input_base);
-    cpi->egpu.gpu_input[gpu_bsize] = gpu_input_base;
+    egpu->acquire_input_buffer(cpi, gpu_bsize, (void **) &gpu_input_base);
 
     for (mi_row = tile->mi_row_start; mi_row < tile->mi_row_end; mi_row +=
         mi_row_step) {
@@ -165,102 +225,22 @@ static void vp9_gpu_fill_mv_input(VP9_COMP *cpi, const TileInfo * const tile) {
           mi_col_step) {
         GPU_INPUT *gpu_input = gpu_input_base
             + get_gpu_buffer_index(cm, mi_row, mi_col, bsize);
-        const int bsl = mi_width_log2(bsize);
-        const int pred_filter_search = cm->interp_filter == SWITCHABLE ?
-            (((mi_row + mi_col) >> bsl) +
-             get_chessboard_index(cm->current_video_frame)) & 0x1 : 0;
+        if (!sf->partition_check) {
+          const int sb_row_index = mi_row / MI_SIZE;
+          const int sb_col_index = mi_col / MI_SIZE;
+          const int sb_index = (cm->sb_cols * sb_row_index + sb_col_index);
+          const int is_static_area = cm->is_background_map[sb_index];
+          const int idx_str = cm->mi_stride * mi_row + mi_col;
+          MODE_INFO **prev_mi = cm->prev_mi_grid_visible + idx_str;
+          assert(prev_mi[0] != NULL);
 
-        const int ms = num_8x8_blocks_wide_lookup[bsize] / 2;
-        const int force_horz_split = (mi_row + ms >= cm->mi_rows);
-        const int force_vert_split = (mi_col + ms >= cm->mi_cols);
-        if (force_horz_split | force_vert_split) {
-          continue;
-        }
-
-        mi_width = num_8x8_blocks_wide_lookup[bsize];
-        mi_height = num_8x8_blocks_high_lookup[bsize];
-
-        vpx_memset(mbmi, 0, sizeof(MB_MODE_INFO));
-
-        vp9_init_plane_quantizers(cpi, x);
-
-        set_mi_row_col(xd, tile, mi_row, mi_height, mi_col, mi_width,
-                       cm->mi_rows, cm->mi_cols);
-
-        mbmi->sb_type = bsize;
-        mbmi->ref_frame[0] = NONE;
-        mbmi->ref_frame[1] = NONE;
-        mbmi->tx_size = MIN(max_txsize_lookup[bsize],
-                            tx_mode_to_biggest_tx_size[cm->tx_mode]);
-
-        mbmi->interp_filter =
-            cm->interp_filter == SWITCHABLE ? EIGHTTAP : cm->interp_filter;
-
-        vp9_find_mv_refs_rt(cm, x, tile, xd->mi[0], ref_frame, candidates,
-                            mi_row, mi_col);
-
-        vp9_find_best_ref_mvs(xd, cm->allow_high_precision_mv, candidates,
-                              (int_mv *)&gpu_input->nearest_mv,
-                              (int_mv *)&gpu_input->near_mv);
-
-        clamp_mv2(&gpu_input->nearest_mv, xd);
-        clamp_mv2(&gpu_input->near_mv, xd);
-
-        gpu_input->do_newmv     = 1;
-        gpu_input->do_compute   = 1;
-        gpu_input->mode_context = mbmi->mode_context[ref_frame];
-
-        if(pred_filter_search)
-          gpu_input->filter_type = SWITCHABLE;
-        else
-          gpu_input->filter_type = EIGHTTAP;
-      }
-    }
-  }
-
-  for (mi_row = tile->mi_row_start; mi_row < tile->mi_row_end; mi_row +=
-      MI_BLOCK_SIZE) {
-    for (mi_col = tile->mi_col_start; mi_col < tile->mi_col_end; mi_col +=
-        MI_BLOCK_SIZE) {
-      const int is_static_area = is_background(cpi, tile, mi_row, mi_col);
-      const int sb_row_index = mi_row / MI_SIZE;
-      const int sb_col_index = mi_col / MI_SIZE;
-      const int sb_index = (cm->sb_cols * sb_row_index + sb_col_index);
-
-      cm->is_background_map[sb_index] = is_static_area;
-
-      if (!sf->partition_check && is_static_area) {
-        for (gpu_bsize = 0; gpu_bsize < GPU_BLOCK_SIZES; ++gpu_bsize) {
-          const BLOCK_SIZE bsize = get_actual_block_size(gpu_bsize);
-          GPU_INPUT *gpu_input_base;
-          const int mi_row_step = num_8x8_blocks_high_lookup[bsize];
-          const int mi_col_step = num_8x8_blocks_wide_lookup[bsize];
-          int block_row, block_col;
-
-          egpu->acquire_input_buffer(cpi, gpu_bsize, (void **)&gpu_input_base);
-
-          for (block_row = 0; block_row < MI_BLOCK_SIZE; block_row +=
-              mi_row_step) {
-            for (block_col = 0; block_col < MI_BLOCK_SIZE; block_col +=
-                mi_col_step) {
-
-              const int actual_mi_row = mi_row + block_row;
-              const int actual_mi_col = mi_col + block_col;
-              const int idx_str = cm->mi_stride * actual_mi_row + actual_mi_col;
-              MODE_INFO **prev_mi = cm->prev_mi_grid_visible + idx_str;
-              if(actual_mi_row >= cm->mi_rows || actual_mi_col >= cm->mi_cols)
-                break;
-              assert(prev_mi[0] != NULL);
-
-              if (prev_mi[0]->mbmi.sb_type != bsize) {
-                GPU_INPUT *gpu_input = gpu_input_base
-                    + get_gpu_buffer_index(cm, actual_mi_row, actual_mi_col,
-                                           bsize);
-                gpu_input->do_compute = 0;
-              }
-            }
+          if (prev_mi[0]->mbmi.sb_type != bsize && is_static_area) {
+            gpu_input->do_compute = 0;
+            continue;
           }
         }
+        vp9_gpu_fill_input_block(cpi, tile, gpu_input, mi_row, mi_col,
+                                 bsize);
       }
     }
   }
